07af43addf3080a0fefad52f4324b0b1cfcbf142
oscar mugambi <oscarmugambi@gmail.com>
2022-09-10 19:34:38 +0300

    added speedLimit

--- src/api/mars-weather.js
+++ src/api/mars-weather.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const axios = require('axios')
 const rateLimit = require('express-rate-limit')
+const slowDown = require('express-slow-down')
 
 const limiter = rateLimit({
   windowMs: 30 * 1000,
-  max: 2,
+  max: 10,
 })
+
+const speedLImiter = slowDown({
+  windowMs: 30 * 1000,
+  delayAfter: 2,
+  delayMs: 500,
+})
+
 const router = express.Router()
 
 const BASE_URL = `https://api.nasa.gov/insight_weather/?`
@@ -13,7 +21,7 @@ const BASE_URL = `https://api.nasa.gov/insight_weather/?`
 let cachedData
 let cacheTime
 
-router.get('/', limiter, async (req, res, next) => {
+router.get('/', speedLImiter, limiter, async (req, res, next) => {
   if (cacheTime && cacheTime > Date.now() - 30 * 1000) {
     return res.json(cachedData)
   }
